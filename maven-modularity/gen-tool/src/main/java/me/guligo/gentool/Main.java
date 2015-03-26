a631c3f471c545cc0938426a02fac9c007c42d06
Igors Gulbinskis <igors.gulbinskis@ericsson.com>
2015-03-26 22:08:38 +0200

    gen-tool: updates

--- maven-modularity/gen-tool/src/main/java/me/guligo/gentool/Main.java
+++ maven-modularity/gen-tool/src/main/java/me/guligo/gentool/Main.java
@@ -12,6 +12,12 @@ import me.guligo.gentool.format.GenTool;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
+		if (args.length < 2 || args.length % 2 != 0) {
+			System.out.println("Usage:");
+			System.out.println("java -jar gen-tool.jar <gen file path> <content dir path> <key 1> <value 1> <key 2> <value 2> ...");
+			System.exit(0);
+		}
+
 		GenTool genFileProcessor = new GenTool(args[0], args[1]);
 		for (int i = 2; i < args.length; i++) {
 			genFileProcessor.setParameter(args[i++], args[i]);
