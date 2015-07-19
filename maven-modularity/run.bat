c5e49d7c5d0079395144295a4e81ad620c87de6a
guligo <igors.gulbinskis@gmail.com>
2015-07-19 23:52:02 +0200

    Updates

--- maven-modularity/run.bat
+++ maven-modularity/run.bat
@@ -1,4 +1,5 @@
 @echo off
+set workdir=%cd%
 
 rem Remove possible left-overs
 
@@ -8,6 +9,7 @@ rem Generate Maven projects
 
 cd generator
 call mvn clean package
+rem set MAVEN_OPTS=-Xmx4096m -XX:MaxPermSize=1024m
 
 rem 1 project with 3 * 9 = 27 classes
 call mvn exec:java -Dexec.mainClass=me.guligo.experiments.mavenmodularity.generator.Main -Dexec.args="flat ../generated stage-1-flat 3 9"
@@ -17,18 +19,18 @@ rem 1 project with 10 * 25 = 250 classes
 call mvn exec:java -Dexec.mainClass=me.guligo.experiments.mavenmodularity.generator.Main -Dexec.args="flat ../generated stage-3-flat 10 25"
 rem 1 project with 10 * 250 = 2500 classes
 call mvn exec:java -Dexec.mainClass=me.guligo.experiments.mavenmodularity.generator.Main -Dexec.args="flat ../generated stage-4-flat 10 250"
-rem 1 project with 10 * 2500 = 25000 classes
-call mvn exec:java -Dexec.mainClass=me.guligo.experiments.mavenmodularity.generator.Main -Dexec.args="flat ../generated stage-5-flat 10 2500"
-rem 1 project, 2 levels - 3 modules per level with 9 classes in each leaf module - 1 * 3 * 9 = 27 classes
-call mvn exec:java -Dexec.mainClass=me.guligo.experiments.mavenmodularity.generator.Main -Dexec.args="tree ../generated stage-1-tree 2 3 9"
-rem 1 project, 2 levels - 5 modules per level with 15 classes in each leaf module - 1 * 5 * 15 = 75 classes
-call mvn exec:java -Dexec.mainClass=me.guligo.experiments.mavenmodularity.generator.Main -Dexec.args="tree ../generated stage-2-tree 2 5 15"
+rem 1 project with 10 * 500 = 5000 classes
+call mvn exec:java -Dexec.mainClass=me.guligo.experiments.mavenmodularity.generator.Main -Dexec.args="flat ../generated stage-5-flat 10 500"
+rem 1 project, 3 levels - 3 modules per level with 3 classes in each leaf module - 1 * 3 * 3 * 3 = 27 classes
+call mvn exec:java -Dexec.mainClass=me.guligo.experiments.mavenmodularity.generator.Main -Dexec.args="tree ../generated stage-1-tree 3 3 3"
+rem 1 project, 3 levels - 5 modules per level with 3 classes in each leaf module - 1 * 5 * 5 * 3 = 75 classes
+call mvn exec:java -Dexec.mainClass=me.guligo.experiments.mavenmodularity.generator.Main -Dexec.args="tree ../generated stage-2-tree 3 5 3"
 rem 1 project, 3 levels - 5 modules per level with 10 classes in each leaf module - 1 * 5 * 5 * 10 = 250 classes
 call mvn exec:java -Dexec.mainClass=me.guligo.experiments.mavenmodularity.generator.Main -Dexec.args="tree ../generated stage-3-tree 3 5 10"
 rem 1 project, 3 levels - 5 modules per level with 100 classes in each leaf module - 1 * 5 * 5 * 100 = 2500 classes
 call mvn exec:java -Dexec.mainClass=me.guligo.experiments.mavenmodularity.generator.Main -Dexec.args="tree ../generated stage-4-tree 3 5 100"
-rem 1 project, 3 levels - 5 modules per level with 1000 classes in each leaf module - 1 * 5 * 5 * 1000 = 25000 classes
-call mvn exec:java -Dexec.mainClass=me.guligo.experiments.mavenmodularity.generator.Main -Dexec.args="tree ../generated stage-5-tree 3 5 1000"
+rem 1 project, 3 levels - 5 modules per level with 200 classes in each leaf module - 1 * 5 * 5 * 200 = 5000 classes
+call mvn exec:java -Dexec.mainClass=me.guligo.experiments.mavenmodularity.generator.Main -Dexec.args="tree ../generated stage-5-tree 3 5 200"
 
 cd ..
 cd generated
@@ -48,7 +50,7 @@ call:buildAndMeasureGeneratedProject stage-5-tree
 
 rem Return to starting point
 
-cd ..
+cd %workdir%
 echo ##########
 type results
 echo ##########
