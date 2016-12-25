cd10bae58e6f7d4854940cb6d98457b9e47772ce
Igors Gulbinskis <igors.gulbinskis@gmail.com>
2016-12-26 00:49:16 +0100

    Minor updates

--- telekom-fon-session-upkeeper/index.js
+++ telekom-fon-session-upkeeper/index.js
@@ -6,9 +6,10 @@ require('fs').readFile('config.json', 'utf8', function(error, content) {
 
     // constants
     const PAUSE_BEFORE_AUTH = 60000;
-    const PAUSE_AFTER_AUTH = 60000;
+    const PAUSE_AFTER_AUTH = 30000;
     const PAUSE_AFTER_SUCCESS = 15000;
-    const PAUSE_AFTER_ERROR = 30000;
+    const PAUSE_AFTER_ERROR = 15000;
+    const PAUSE_AFTER_RSSI = 60000;
 
     const TARGET = config.target;
     const USERNAME = config.username;
@@ -22,36 +23,19 @@ require('fs').readFile('config.json', 'utf8', function(error, content) {
     // misc
     const sleep = require('sleep');
     const colors = require('colors/safe');
+    const scanner = require('node-wifi-scanner');
 
     var upkeepSession = function() {
         var pageLoadStartTime = Date.now();
         browser
             .fetch(TARGET)
             .then(function(response) {
-                if (response.request.url.indexOf('telekom.portal.fon.com') > -1) {
+                if (isLoginPortal(response)) {
                     browser
                         .visit(response.request.url, function() {
                             var pageLoadEndTime = Date.now();
                             var pageLoadTime = (pageLoadEndTime - pageLoadStartTime) / 1000;
                             console.log(colors.yellow('Login portal was hit, loading time = [' + pageLoadTime + 's]'));
-
-                            var authenticateSession = function() {
-                                try {
-                                    browser.fill('username', USERNAME);
-                                    browser.fill('password', PASSWORD);
-
-                                    // just making sure that certain libs have been loaded on authentication page
-                                    browser.evaluate("jQuery('#username').val()");
-
-                                    browser.clickLink('Verbindung herstellen', function() {
-                                        console.log(colors.yellow('Authentication completed'));
-
-                                        setTimeout(upkeepSession, PAUSE_AFTER_AUTH);
-                                    });
-                                } catch (error) {
-                                    handleError(error);
-                                }
-                            }
 
                             setTimeout(authenticateSession, PAUSE_BEFORE_AUTH);
                         })
@@ -64,7 +48,52 @@ require('fs').readFile('config.json', 'utf8', function(error, content) {
                 }
             })
             .catch(handleError);
-    };
+    }
+
+    var isLoginPortal = function(response) {
+        return response.request.url.indexOf('telekom.portal.fon.com') > -1;
+    }
+
+    var authenticateSession = function() {
+        try {
+            browser.fill('username', USERNAME);
+            browser.fill('password', PASSWORD);
+
+            // just making sure that certain libs have been loaded on authentication page
+            browser.evaluate("jQuery('#username').val()");
+
+            browser.clickLink('Verbindung herstellen', function() {
+                console.log(colors.yellow('Authentication completed'));
+
+                setTimeout(upkeepSession, PAUSE_AFTER_AUTH);
+            });
+        } catch (error) {
+            handleError(error);
+        }
+    }
+
+    var checkSignalStrength = function(predicate) {
+        scanner.scan((err, networks) => {
+            for (i = 0; i < networks.length; i++) {
+                var network = networks[i];
+                if (network.ssid === 'Telekom_FON') {
+                    var msg = 'Signal strength of AP with MAC [' + network.mac + '] is [' + network.rssi + ' dBm]';
+                    logSignalStrength(network.rssi, msg);
+                }
+            }
+            return;
+        });
+    }
+
+    var logSignalStrength = function(rssi, msg) {
+        if (rssi > -80) {
+            console.log(colors.green(msg));
+        } else if (rssi > -90) {
+            console.log(colors.yellow(msg));
+        } else {
+            console.log(colors.red(msg));
+        }
+    }
 
     var handleError = function(error) {
         console.log(colors.red('Connection error occurred'));
@@ -73,6 +102,7 @@ require('fs').readFile('config.json', 'utf8', function(error, content) {
         setTimeout(upkeepSession, PAUSE_AFTER_ERROR);
     }
 
+    setInterval(checkSignalStrength, PAUSE_AFTER_RSSI);
     upkeepSession();
 
 });
